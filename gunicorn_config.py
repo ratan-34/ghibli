ab8d5ecf55f08ebecac90674622de05740ca0b7a
ratan-34 <hackerrahul819@gmail.com>
2025-04-09 04:29:18 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,50 +1,70 @@
-# Gunicorn configuration for Render
 import os
+import multiprocessing
 
 # Server socket
-bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
+bind = "0.0.0.0:" + os.environ.get("PORT", "5000")
 backlog = 2048
 
 # Worker processes
 workers = 1
-worker_class = 'sync'
+worker_class = 'gthread'
+threads = 4
 worker_connections = 1000
 timeout = 120
 keepalive = 2
 
-# Process naming
-proc_name = 'gunicorn_animegan'
+# Security
+limit_request_line = 4094
+limit_request_fields = 100
+limit_request_field_size = 8190
 
-# Server mechanics
-daemon = False
-pidfile = None
-umask = 0
-user = None
-group = None
-tmp_upload_dir = None
+# Debugging
+reload = False
+reload_engine = 'auto'
+reload_extra_files = []
 
 # Logging
+accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
-accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
-# Process management
-max_requests = 1000
-max_requests_jitter = 50
+# Process naming
+proc_name = 'ghibli_style_transfer'
 
 # Server hooks
 def post_fork(server, worker):
-    server.log.info("Worker spawned (pid: %s)", worker.pid)
-
-def pre_exec(server):
-    server.log.info("Forked child, re-executing.")
+    server.log.info("Worker %s spawned", worker.pid)
 
 def when_ready(server):
     server.log.info("Server is ready. Spawning workers")
 
 def worker_int(worker):
-    worker.log.info("worker received INT or QUIT signal")
+    worker.log.info("Worker received INT or QUIT signal")
 
 def worker_abort(worker):
-    worker.log.info("worker received SIGABRT signal")
+    worker.log.info("Worker received SIGABRT signal")
+
+def pre_fork(server, worker):
+    pass
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+def on_starting(server):
+    pass
+
+def on_reload(server):
+    pass
+
+def child_exit(server, worker):
+    pass
+
+def worker_exit(server, worker):
+    pass
+
+def nworkers_changed(server, new_value, old_value):
+    pass
+
+def on_exit(server):
+    pass
